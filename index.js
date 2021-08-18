0c8e83aa147d56ef30643011aa1dbd9dea7155c1
Sustanza <422661+sustanza@users.noreply.github.com>
2021-08-18 17:54:43 -0500

    minor cleanup and exception handling
    - removes some basic dev performance time checking
    - wraps some important code in try catch for some basic exception handling

--- index.js
+++ index.js
@@ -1,46 +1,56 @@
 const { main } = require('./main.js')
 const fs = require('fs')
 
-// check if required arguments are sent
-if (process.argv.length < 5) {
-    console.log(
-        `Arguments missing!\nPlease try again using the following syntax:\nnode index.js "slcpsCSVFilePath.csv" "zipsCSVFilePath.csv" "plansCSVFilePath.csv"`
+try {
+    // check if required arguments are sent
+    if (process.argv.length < 5) {
+        console.log(
+            `Arguments missing!\nPlease try again using the following syntax:\nnode index.js "slcpsCSVFilePath.csv" "zipsCSVFilePath.csv" "plansCSVFilePath.csv"`
+        )
+        // exit program with error exit code
+        process.exit(1)
+    }
+
+    // parse out required args
+    const slcpsRecordsPath = process.argv[2]
+    const zipsRecordsPath = process.argv[3]
+    const planRecordsPath = process.argv[4]
+
+    // optional arg
+    const slcpsFinalRecordsPath = process.argv[5]
+
+    // check if SLCPS path argument is present
+    if (!fs.existsSync(slcpsRecordsPath)) {
+        console.log(
+            `The file at the file path ${slcpsRecordsPath} to SLCPS CSV does not exist.`
+        )
+        process.exit(1)
+    }
+
+    // check if SLCPS path argument is present
+    if (!fs.existsSync(zipsRecordsPath)) {
+        console.log(
+            `The file at the file path ${zipsRecordsPath} to zips CSV does not exist.`
+        )
+        process.exit(1)
+    }
+
+    // check if SLCPS path argument is present
+    if (!fs.existsSync(planRecordsPath)) {
+        console.log(
+            `The file at the file path ${planRecordsPath} to plans CSV does not exist.`
+        )
+        process.exit(1)
+    }
+
+    // execute the main program
+    main(
+        slcpsRecordsPath,
+        zipsRecordsPath,
+        planRecordsPath,
+        slcpsFinalRecordsPath
     )
-    // exit program with error exit code
+} catch (ex) {
+    // do something with the exception at some point
     process.exit(1)
 }
-
-// parse out required args
-const slcpsRecordsPath = process.argv[2]
-const zipsRecordsPath = process.argv[3]
-const planRecordsPath = process.argv[4]
-
-// optional arg
-const slcpsFinalRecordsPath = process.argv[5]
-
-// check if SLCPS path argument is present
-if (!fs.existsSync(slcpsRecordsPath)) {
-    console.log(
-        `The file at the file path ${slcpsRecordsPath} to SLCPS CSV does not exist.`
-    )
-    process.exit(1)
-}
-
-// check if SLCPS path argument is present
-if (!fs.existsSync(zipsRecordsPath)) {
-    console.log(
-        `The file at the file path ${zipsRecordsPath} to zips CSV does not exist.`
-    )
-    process.exit(1)
-}
-
-// check if SLCPS path argument is present
-if (!fs.existsSync(planRecordsPath)) {
-    console.log(
-        `The file at the file path ${planRecordsPath} to plans CSV does not exist.`
-    )
-    process.exit(1)
-}
-
-// execute program
-main(slcpsRecordsPath, zipsRecordsPath, planRecordsPath, slcpsFinalRecordsPath)
--- main.js
+++ main.js
@@ -12,47 +12,40 @@ const main = async (
     planFilePath,
     slcpsFinalFilePath
 ) => {
-    // create a date object for start time performance analysis
-    const start = new Date()
+    try {
+        // parse CSV files to an array of objects that represent the CSV rows using columns as mapped properties
+        let slcpsRecords = await slcsp.parseCSVFile(slcpsFilePath)
+        const zipsRecords = await slcsp.parseCSVFile(zipsFilePath)
+        const planRecords = await slcsp.parseCSVFile(planFilePath)
 
-    // parse CSV files to an array of objects that represent the CSV rows using columns as mapped properties
-    let slcpsRecords = await slcsp.parseCSVFile(slcpsFilePath)
-    const zipsRecords = await slcsp.parseCSVFile(zipsFilePath)
-    const planRecords = await slcsp.parseCSVFile(planFilePath)
+        // create a map data structure to help manage and organize data as associations are made from all of the data sources
+        let zipDataMap = slcsp.createZipDataMap(slcpsRecords)
 
-    // create a map data structure to help manage and organize data as associations are made from all of the data sources
-    let zipDataMap = slcsp.createZipDataMap(slcpsRecords)
+        // associates the rate areas between the zip codes we need rates for and all zip codes using the zip records data source
+        await slcsp.associateZipToRateArea(zipDataMap, zipsRecords)
 
-    // associates the rate areas between the zip codes we need rates for and all zip codes using the zip records data source
-    await slcsp.associateZipToRateArea(zipDataMap, zipsRecords)
+        // associates the rate areas with plan rates using the plan records data source
+        await slcsp.associateRateAreaWithPlanRate(zipDataMap, planRecords)
 
-    // associates the rate areas with plan rates using the plan records data source
-    await slcsp.associateRateAreaWithPlanRate(zipDataMap, planRecords)
+        // sets the data rate for zips in our SLCPS records from our zip map data structure
+        await slcsp.setSLCPSRate(zipDataMap, slcpsRecords)
 
-    // sets the data rate for zips in our SLCPS records from our zip map data structure
-    await slcsp.setSLCPSRate(zipDataMap, slcpsRecords)
+        // converts the SLCPS records back to a CSV string so we can print it and write it to disk
+        const csvString = await slcsp.convertRecordsToCSVString(slcpsRecords)
 
-    // converts the SLCPS records back to a CSV string so we can print it and write it to disk
-    const csvString = await slcsp.convertRecordsToCSVString(slcpsRecords)
+        // determines the final record path based on if optional path is provided or to use the original SLCPS record path
+        let finalRecordsPath = slcpsFinalFilePath
+            ? slcpsFinalFilePath
+            : slcpsRecordsPath
 
-    // determines the final record path based on if optional path is provided or to use the original SLCPS record path
-    let finalRecordsPath = slcpsFinalFilePath
-        ? slcpsFinalFilePath
-        : slcpsRecordsPath
+        // write CSV string to disk as per the requirements
+        await slcsp.writeCSVStringToFile(finalRecordsPath, csvString)
 
-    // write CSV string to disk as per the requirements
-    await slcsp.writeCSVStringToFile(finalRecordsPath, csvString)
-
-    // prints CSV string to console as per the requirements
-    slcsp.printString(csvString)
-
-    // create a date object for stop time performance analysis
-    const stop = new Date()
-
-    // print the total execution time for performance analysis
-    console.log(
-        `Executing time = ${(stop.getTime() - start.getTime()) / 1000} seconds`
-    )
+        // prints CSV string to console as per the requirements
+        slcsp.printString(csvString)
+    } catch (ex) {
+        // do something with the exception
+    }
 }
 
 module.exports = { main }
